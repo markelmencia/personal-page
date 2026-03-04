4891be2772db7b65e1f634e0bef7056dd12f4a5c
Markel Mencía <markel.mnc@gmail.com>
2026-03-04 21:14:19 +0100

    Add post images to post descriptions

--- components/PostDescription.js
+++ components/PostDescription.js
@@ -1,15 +1,25 @@
+import Image from "next/image"
 import Link from "next/link"
 
-export default function PostDescription({title, date, desc, slug, type}) {
+export default function PostDescription({title, date, desc, slug, type, img, alt}) {
   return (
     <div className="post-desc">
          <Link href={`/${type}/${slug}`}>
+            <div className="desc">
+              {img && (
+              <img className="article-image"
+                src={img}
+                alt={alt}>
+
+              </img>
+          )}
               <div className="desc-text">
                 <p className="desc-date">{date}</p>
-               <h1 className="desc-title">{title}</h1>
-               <p className="desc-desc">{desc}</p>
-          </div>
-            </Link>
+                <h1 className="desc-title">{title}</h1>
+                <p className="desc-desc">{desc}</p>
+              </div>
+            </div>
+          </Link>        
     </div>
   )
 }
